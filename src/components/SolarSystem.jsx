5f3a127b5a91372fe5b146025967397dbe7e270b
Yatharth Verma <yatharthverma@Yatharths-MacBook-Pro.local>
2023-08-31 22:50:50 +0530

    added planet but animation is pending

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import P1 from "./../assets/P1.png";
+import { motion } from "framer-motion";
 
 function SolarSystem() {
   return (
@@ -7,7 +9,16 @@ function SolarSystem() {
         <div className="w-[850px] h-[850px] border border-slate-600 border-t-0 rounded-full bg-transparent flex flex-col justify-center items-center">
           <div className="w-[700px] h-[700px] border border-slate-600 border-t-0 rounded-full bg-transparent flex flex-col justify-center items-center">
             <div className="w-[550px] h-[550px] border border-slate-600 border-t-0 rounded-full bg-transparent flex flex-col justify-center items-center">
-              <div className="w-[400px] h-[400px] border border-slate-600 border-t-0 rounded-full bg-transparent flex flex-col justify-center items-center"></div>
+              <div className="w-[400px] h-[400px] border border-slate-600 border-t-0 rounded-full bg-transparent flex flex-col justify-center items-center relative">
+                <motion.img
+                  src={P1}
+                  className="w-24 h-24 absolute top-0"
+                  animate={{
+                    x: [0, -10, -20, -30, -40, -50, -60, -70],
+                    y: [350, 340, 330, 320, 310, 300, 290],
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
